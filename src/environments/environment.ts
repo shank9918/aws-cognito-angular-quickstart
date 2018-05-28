535ecaf0d38e2b9ab733665d31231a5fdeff2421
Shashank Gowri <shashankgowri@gmail.com>
2018-05-28 20:28:08 +0530

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: false,
+
+    region: 'ap-south-1',
+
+    identityPoolId: 'ap-south-1:382e8b81-50c0-4651-8a00-909e8c4c708e',
+    userPoolId: 'ap-south-1_y6AS1g7AS',
+    clientId: '483q7d79ei19i165o2fm0kumi2',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'ap-south-1',
+
+    ddbTableName: 'bmc_logintrials',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+};
+
